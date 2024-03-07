5b111f4a309d478643da04afa9c907e1519f4cca
queenoko <quimaudongwo@gmail.com>
2024-03-07 13:09:54 +0100

    simple test route

--- server.js
+++ server.js
@@ -9,11 +9,18 @@ var session = require('express-session')
 
 const app = express()
 // Middle wares
+app.use(express.static('public'))
 app.session('ejs', engine)
 app.set('view engine', 'ejs')
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+
+// Route Methods
+app.get('/', (req, res) => {
+  res.send('index')
+})
 
 
 // run server
