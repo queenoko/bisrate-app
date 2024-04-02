30f1c592676f8cec13cb037aa5db0c0a3f9deaa8
queenoko <quimaudongwo@gmail.com>
2024-04-02 16:40:06 +0100

    file upload

---
+++ public/js/upload.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    $('.upload-btn').on('click', function(){
+        $('#upload-input').click();
+        
+        $('.progress-bar').text('0%');
+        $('.progress-bar').width('0%');
+    });
+})
